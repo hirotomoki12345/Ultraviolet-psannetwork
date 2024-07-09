7a148dcaa7d87b5c1c62f0ba049188c91f54201a
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-07-09 11:13:10 +0900

    Update youtube_dl.js

--- public/youtube_dl.js
+++ public/youtube_dl.js
@@ -1,5 +1,5 @@
         function youtube_dl() {
-            const url = document.getElementById('inputText').value;
+            const url = document.getElementById('youtubeUrl').value;
             if (url) {
                 window.location.href = `/download?url=${encodeURIComponent(url)}`;
             } else {
