6960e810074e62d6a1ccc0743312e21fbf020f88
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-07-09 10:09:04 +0900

    Update and rename youtube-skip.js to youtube_dl.js

--- public/youtube-skip.js
+++
@@ -1,8 +0,0 @@
-setInterval(()=>{
-  const adOverlay = document.querySelector('.ytp-ad-overlay-close-container');
-  const skipButton = document.querySelector('.ytp-ad-skip-button');
-  if(adOverlay !=undefined)
-    adOverlay.click();
-  if(skipButton != undefined)
-    skipButton.click();
-}, 2000);
